8ca01dc5dc630520f9302a581ea3b90e3ad215ef
agark0vlad21 <72999843+agark0vlad21@users.noreply.github.com>
2022-10-17 14:57:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-import random, string, requests, urllib, os
+token = "" # token of your bot
+url = "https://api.telegram.org/bot"
+channel_id = "" # username or id of channel
+url += token
+method = url + "/sendPhoto"
+
 
 def generate_alphanum_random_string(length):
     letters_and_digits = string.ascii_letters + string.digits
     rand_string = ''.join(random.sample(letters_and_digits, length))
-    variable = f"https://i.imgur.com/{rand_string}.jpg"
-    #print(variable)
-    response = requests.get(variable)
+    link = f"https://i.imgur.com/{rand_string}.jpg"
+    #print(link)
+    response = requests.get(link)
     if response.history:
         print("url is not valid")
     else:
         print("valid url, downloading image")
-        urllib.request.urlretrieve(variable,rand_string)
-        # if you want to send image as file, change sendasimage to sendasfile.sh
-        os.system(f'sleep 1 && mv {rand_string} {rand_string}.jpg && bash sendasimage.sh {rand_string}.jpg > /dev/null')
+        requests.post(method, data={ "chat_id": channel_id, "photo": link })
+        time.sleep(1)
 while True:
     generate_alphanum_random_string(5)
