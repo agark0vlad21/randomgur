c27b95cea4e5c61ff160aea7ea036f486e302ac1
agark0vlad21 <72999843+agark0vlad21@users.noreply.github.com>
2022-11-26 18:27:48 +0300

    update
    added some optimizations + protection from the fool

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
-import random
-import string
-from requests import get, post
+#! /usr/bin/python3
+
+from random import sample
+from string import digits, ascii_letters
+try:
+    from requests import get, post
+except ModuleNotFoundError:
+    print("looks like you don't install requests library, try to install it with 'pip install requests'")
+    exit(1)
 from time import sleep as wait
 
 TOKEN = "" # token of your bot
-URL = "https://api.telegram.org/bot"
-CHAT_ID = "" # username or id of chat
-URL += TOKEN
-METHOD = URL + "/sendPhoto"
+CHAT_ID = "" # chat id to send photos, like @telegram, or -1825122896521
+METHOD = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
 
-def try_send(length):
+if TOKEN.strip() == "":
+    print("'TOKEN' must be not empty, please fill this field")
+    exit(2)
+elif CHAT_ID.strip() == "":
+    print("'CHAT_ID' must be not empty, please fill this field")
+    exit(3)
+
+def try_send_with(length):
     try:
-        letters_and_digits = string.ascii_letters + string.digits
-        rand_string = ''.join(random.sample(letters_and_digits, length))
-        link = f"https://i.imgur.com/{rand_string}.jpg"
-        response = get(link)
-        if response.history:
+        letters_and_digits = ascii_letters + digits
+        image_id = ''.join(sample(letters_and_digits, length))
+        if get(f"https://i.imgur.com/{image_id}.jpg").history:
             print("\u001b[31murl is not valid\u001b[0m")
         else:
             print("\u001b[32mgot valid url, sending image\u001b[0m")
-            post(METHOD, data={ "chat_id": CHAT_ID, "photo": link })
+            post(METHOD, data={ "chat_id": CHAT_ID, "photo": f"https://i.imgur.com/{image_id}.jpg" })
             wait(1)
     except (EOFError, KeyboardInterrupt):
-            print ("\n\u001b[36mBye!\u001b[0m")
+            print ("\u001b[2D\u001b[36mBye!\u001b[0m")
             exit(0)
     except Exception:
             print ("\u001b[31;1merror has been occured when downloading or checking image\u001b[0m")
             wait(1)
 while True:
-    try_send(5)
+    try_send_with(5)
