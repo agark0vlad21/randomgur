bc5e060be0fa7bed760896e3b4cf7d8e17ceb701
agark0vlad21 <72999843+agark0vlad21@users.noreply.github.com>
2022-11-27 16:37:22 +0300

    Update main.py
    updated protection from fool

--- main.py
+++ main.py
@@ -9,18 +9,21 @@ TOKEN = "" # token of your bot
 CHAT_ID = "" # chat id for send photos, like @telegram, or -1825122896521
 METHOD = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
 
-if TOKEN.strip() == "":
-    print("'TOKEN' must be not empty, please fill this field")
-    exit(2)
-elif CHAT_ID.strip() == "":
-    print("'CHAT_ID' must be not empty, please fill this field")
-    exit(3)
-
+if TOKEN.strip() == "" or CHAT_ID == "":
+    print(" 'TOKEN' and 'CHAT_ID' variables must be not empty")
+    exit(1)
+else:
+    print("trying to check posting automatically, please wait")
+    if not post(METHOD, data={ "chat_id": CHAT_ID,
+                              "photo": "http://http.cat/200",
+                              "caption": "test photo"  }).ok:
+        print("looks like you configure randomgur incorrect, please check 'CHAT_ID' and 'TOKEN' variables\nNOTE: you need to issue the rights of the administrator of the bot if the posts go to the channel")
+        exit(2)
 def try_send_with(length):
     try:
         letters_and_digits = ascii_letters + digits
         image_id = ''.join(sample(letters_and_digits, length))
-        if get(f"https://i.imgur.com/{image_id}.jpg").history:
+        if get(f"https://i.imgur.com/{image_id}.jpg", allow_redirects=False).history:
             print("\u001b[31murl is not valid\u001b[0m")
         else:
             print("\u001b[32mgot valid url, sending image\u001b[0m")
